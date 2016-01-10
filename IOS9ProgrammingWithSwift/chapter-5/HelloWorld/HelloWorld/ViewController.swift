c6a62f2248a4ca5fa70f1804d2fc78210a3ea45f
JasonChiu <JasonChiuCC@gmail.com>
2016-01-10 23:17:49 +0800

    IOS9ProgrammingWithSwift_CH5_AutoLayout

---
+++ IOS9ProgrammingWithSwift/chapter-5/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Simon Ng on 5/8/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+}
+
