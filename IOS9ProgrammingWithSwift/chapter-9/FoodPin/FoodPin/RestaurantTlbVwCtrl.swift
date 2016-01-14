70a9586f1736a38c99c820b2efd76cd9415c104c
JasonChiu <JasonChiuCC@gmail.com>
2016-01-14 21:48:16 +0800

    IOS9ProgrammingWithSwift_CH9_CustomTableView
    顯示不同圖片

--- IOS9ProgrammingWithSwift/chapter-9/FoodPin/FoodPin/RestaurantTlbVwCtrl.swift
+++ IOS9ProgrammingWithSwift/chapter-9/FoodPin/FoodPin/RestaurantTlbVwCtrl.swift
@@ -12,6 +12,8 @@ class RestaurantTlbVwCtrl: UITableViewController {
 
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Thai Cafe"]
     
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "thaicafe.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -38,7 +40,7 @@ class RestaurantTlbVwCtrl: UITableViewController {
         
         // 設定 cell
         cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant.jpg")
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
         return cell
     }
 
