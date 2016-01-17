161842743d3a8db0da7ead0c3fbf1d249981b21f
JasonChiu <JasonChiuCC@gmail.com>
2016-01-18 00:55:14 +0800

    AppCoda 並行程式設計
    使用並行/串行隊列來下載圖片及更新 UI

--- AppCoda/ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
+++ AppCoda/ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg",
+                 "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg",
+                 "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg",
+                 "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
 
 class Downloader {
-    
     class func downloadImageWithURL(url:String) -> UIImage! {
-        
         let data = NSData(contentsOfURL: NSURL(string: url)!)
         return UIImage(data: data!)
     }
@@ -41,19 +42,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
     @IBAction func didClickOnStart(sender: AnyObject) {
         
-        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
-        self.imageView1.image = img1
+        // dispatch_get_global_queue 取得 Default 優先權的[並行隊列]
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_async(queue) { () -> Void in
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            
+            // dispatch_get_main_queue 取得[串列隊列]並更新 UI 畫面
+            dispatch_async(dispatch_get_main_queue(), {
+                self.imageView1.image = img1
+            })
+        }
         
-        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
-        self.imageView2.image = img2
+        dispatch_async(queue) { () -> Void in
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.imageView2.image = img2
+            })
+        }
         
-        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
-        self.imageView3.image = img3
+        dispatch_async(queue) { () -> Void in
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.imageView3.image = img3
+            })
+        }
         
-        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
-        self.imageView4.image = img4
+        dispatch_async(queue) { () -> Void in
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            dispatch_async(dispatch_get_main_queue(), {
+                self.imageView4.image = img4
+            })
+        }
         
     }
     @IBAction func sliderValueChanged(sender: UISlider) {
