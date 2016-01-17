bdc6344c1b875ac7551e8fc07de5f06d75731711
JasonChiu <JasonChiuCC@gmail.com>
2016-01-18 01:36:30 +0800

    AppCoda 並行程式設計
    使用 Operation 隊列

--- AppCoda/ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
+++ AppCoda/ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
@@ -13,6 +13,9 @@ let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-t
                  "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg",
                  "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
 
+// 建立 Operation 隊列
+var queue = NSOperationQueue()
+
 class Downloader {
     class func downloadImageWithURL(url:String) -> UIImage! {
         let data = NSData(contentsOfURL: NSURL(string: url)!)
@@ -80,7 +83,9 @@ class ViewController: UIViewController {
                 self.imageView4.image = img4
             })
         }
-#else
+#endif
+        
+#if false
 // 使用新建串行隊列（下載）+ 原始串行隊列（更新UI）
     
         // 建立新的串行隊列
@@ -116,6 +121,41 @@ class ViewController: UIViewController {
                 self.imageView4.image = img4
             })
         }
+#endif
+        
+#if true
+// 使用 Operation 隊列
+    
+    // 使用 addOperationWithBlock 指定的一個閉包來創建一個新的 Operation 實例
+    queue.addOperationWithBlock { () -> Void in
+        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+        
+        // NSOperationQueue.mainQueue() 代替 GCD 中的 dispatch_async() 方法來獲取主隊列
+        NSOperationQueue.mainQueue().addOperationWithBlock({
+            self.imageView1.image = img1
+        })
+    }
+    
+    queue.addOperationWithBlock { () -> Void in
+        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+        NSOperationQueue.mainQueue().addOperationWithBlock({
+            self.imageView2.image = img2
+        })
+    }
+    
+    queue.addOperationWithBlock { () -> Void in
+        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+        NSOperationQueue.mainQueue().addOperationWithBlock({
+            self.imageView3.image = img3
+        })
+    }
+    
+    queue.addOperationWithBlock { () -> Void in
+        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+        NSOperationQueue.mainQueue().addOperationWithBlock({
+            self.imageView4.image = img4
+        })
+    }
 #endif
     }
     
