891446c959aa23638efee206fe5231ab8c63a768
JasonChiu <JasonChiuCC@gmail.com>
2016-01-20 22:00:52 +0800

    IOS9ProgrammingWithSwift_CH14_CustomDetail
    加入詳細 Cell 類別

---
+++ IOS9ProgrammingWithSwift/chapter-14/FoodPin/FoodPin/RestaurantDetailTlbVwCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantDetailTlbVwCell.swift
+//  FoodPin
+//
+//  Created by dah.com on 2016/1/20.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTlbVwCell: UITableViewCell {
+
+    @IBOutlet weak var fieldLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
