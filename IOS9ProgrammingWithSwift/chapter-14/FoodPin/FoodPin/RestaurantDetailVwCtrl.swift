8ee700186bc6cbe1bf58c6eb68d23e68eb0ab089
JasonChiu <JasonChiuCC@gmail.com>
2016-01-20 22:11:39 +0800

    IOS9ProgrammingWithSwift_CH14_CustomDetail
    完成詳細 Cell

--- IOS9ProgrammingWithSwift/chapter-14/FoodPin/FoodPin/RestaurantDetailVwCtrl.swift
+++ IOS9ProgrammingWithSwift/chapter-14/FoodPin/FoodPin/RestaurantDetailVwCtrl.swift
@@ -33,3 +33,41 @@ class RestaurantDetailVwCtrl: UIViewController {
     }
     
 }
+
+extension RestaurantDetailVwCtrl:UITableViewDataSource,UITableViewDelegate {
+    // MARK: - TlbVwDataSource（提供表格資料）
+    /* TLB 中每個區段有幾列（Row） */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    /* TLB 在顯示時會被呼叫 */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId  = "Cell"
+        let cell    = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath) as! RestaurantDetailTlbVwCell
+        
+        // 設定 cell
+        switch (indexPath.row) {
+        case 0:
+            cell.fieldLabel.text = "名稱"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.fieldLabel.text = "類型"
+            cell.valueLabel.text = restaurant.type
+        case 2:
+            cell.fieldLabel.text = "地點"
+            cell.valueLabel.text = restaurant.location
+        case 3:
+            cell.fieldLabel.text = "是否來過"
+            cell.valueLabel.text = restaurant.isVisited ? "來過了" : "沒來過"
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+        }
+
+        // 設定圖片圓角
+        //cell.thumbnailImageView.layer.cornerRadius  = 30.0
+        //cell.thumbnailImageView.clipsToBounds       = true
+        return cell
+    }
+}
