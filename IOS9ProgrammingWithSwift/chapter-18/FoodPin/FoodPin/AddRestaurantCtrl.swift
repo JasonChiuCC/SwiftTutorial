857f2bb51543fb8b9465b4ff76d4504d8d88a5f7
JasonChiu <JasonChiuCC@gmail.com>
2016-03-23 20:17:47 +0800

    IOS9ProgrammingWithSwift_CH18_UIImagePickerCtrl
    加入 Save 按鈕

--- IOS9ProgrammingWithSwift/chapter-18/FoodPin/FoodPin/AddRestaurantCtrl.swift
+++ IOS9ProgrammingWithSwift/chapter-18/FoodPin/FoodPin/AddRestaurantCtrl.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class AddRestaurantCtrl: UITableViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView            : UIImageView!
+    @IBOutlet weak var nameTextField        : UITextField!
+    @IBOutlet weak var typeTextField        : UITextField!
+    @IBOutlet weak var locationTextField    : UITextField!
+    @IBOutlet weak var yesButton            : UIButton!
+    @IBOutlet weak var noButton             : UIButton!
+    var isVisited = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,62 +49,43 @@ class AddRestaurantCtrl: UITableViewController {
         }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
-
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func toggleBeenHereButton(sender: UIButton) {
+        // Yes button clicked
+        if sender == yesButton {
+            isVisited = true
+            yesButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+            noButton.backgroundColor = UIColor.grayColor()
+        } else if sender == noButton {
+            isVisited = false
+            yesButton.backgroundColor = UIColor.grayColor()
+            noButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func save(sender: AnyObject) {
+        let name        = nameTextField.text
+        let type        = typeTextField.text
+        let location    = locationTextField.text
+        
+        // Validate input fields
+        if name == "" || type == "" || location == "" {
+            let alertController = UIAlertController(title: "喔喔！", message: "有些欄位沒有填寫", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "確定", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        // Print input data to console
+        print("名稱: \(name)")
+        print("類型: \(type)")
+        print("地點: \(location)")
+        print("是否來過: \(isVisited)")
+        
+        // Dismiss the view controller
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 // 使用代理目的是要和 UIImagePickerController 實體溝通
